1fc094b4ba4ef103f66ef08b2f05662691be8bec
npiper <solveapuzzledev@gmail.com>
2018-10-11 17:46:19 +0100

    Updated project resources

---
+++ src/main/resources/archetype-resources/gitsetup.sh
@@ -0,0 +1,32 @@
+#/bin/sh
+
+# Check if our github project is set up
+response=$(curl --write-out %{http_code} --silent --output /dev/null https://github.com/npiper/${projectName})
+
+# Check status code
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "${projectName} repository has not been created"
+  exit 1;
+fi
+
+# Initialise repository
+git init
+git add . && git commit -am "initial commit"
+git remote add origin https://github.com/npiper/${projectName}.git
+git push origin
+
+# Create 'develop' branch & push to repo
+git branch develop
+git checkout develop
+git add .
+git commit -m "adding a change from the develop branch"
+git checkout master
+git push origin develop
+
+# create gh-pages branch
+git checkout --orphan gh-pages
+git rm -rf .
+touch README.md
+git add README.md
+git commit -m 'initial gh-pages commit'
+git push origin gh-pages
