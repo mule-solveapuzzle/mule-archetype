42ee6550fca6610a3d5e923a11f442d0d9a21c33
npiper <solveapuzzledev@gmail.com>
2018-10-13 02:24:00 +0100

    Updated archetype

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -4,10 +4,11 @@ package $package;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  /** Increments a function.
+   * 
+   */
+  public int increment(int i) {
+    return i + 1;
+  }
 }
--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -1,20 +1,20 @@
 package $package;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldIncrement() {
+    
+    App a = new App();
+    int result = a.increment(3);
+    assertEquals(4,result);
+  }
 }
